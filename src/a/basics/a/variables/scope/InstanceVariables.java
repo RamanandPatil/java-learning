24f6f5c4b2d01997a6dcaca4d25c071ef72caf25
RamanandPatil <ramanand.patil@gmail.com>
2020-07-07 10:11:37 +0530

    Variables scope: Partially completed for instance variables. Adding still some important notes.

--- src/a/basics/a/variables/scope/InstanceVariables.java
+++ src/a/basics/a/variables/scope/InstanceVariables.java
@@ -1,41 +1,109 @@
 package a.basics.a.variables.scope;
 
 /**
- * Class to demo Instance Variables and their scope:
- * <ul><li><strong>Instance Variables (Non-Static Fields)</strong> Technically
+ * Class to demo Instance Variables and their scope:<br /><br />
+ * <strong>Instance Variables (Non-Static Fields)</strong> Technically
  * speaking, objects store their individual states in "non-static fields", that
  * is, fields declared without the <code>static</code> keyword. Non-static
  * fields are also known as <i>instance variables</i> because their values are
  * unique to each <i>instance</i> of a class (to each object, in other words);
  * the <code>currentSpeed</code> of one bicycle is independent from the
- * <code>currentSpeed</code> of another.</li></ul>
+ * <code>currentSpeed</code> of another.<br /><br />
+ * <strong>Some important rules to remember:</strong>
+ * <ul><li>
+ *     Methods:
+ *     <ul>
+ *         <li>Instance methods cannot be called from static methods without
+ *         creating an instance of the class. But the vice versa is true,
+ *         i.e. static methods can be called directly from instance method
+ *         without creating an instance of a class </li>
+ *         <li>While calling(invoking) static or instance method from outside
+ *         the class which holds them, it is required to use
+ *         dot notation. To be precise, static methods should be called like
+ *         ClassName.staticMethodName() and instance methods should be called
+ *         by instanceName.instanceMethodName()</li>
+ *         <li>While calling(invoking) static or instance method within the
+ *         same class which holds them, it is not required..., but this or
+ *         className can be used.
+ *         this and classname are also optional</li>
+ *     </ul>
+ * </li>
+ * <li>
+ *     Variables:
+ *     <ul>
+ *
+ *     </ul>
+ * </li>
+ * </ul>
  */
 public class InstanceVariables {
-    // Private scope means, this variable can be accesses anywhere within the
-    // class. (Any method, any block of code) but not outside the class.
+    /*
+     * Private scope means, this variable can be accesses anywhere within the
+     *  class. (Any method or  block of code) but not outside the class.
+     * Not accessible outside the current class:
+     *  - Even if the other class is in same package or different package
+     *  - Even if the class is a child class of this class
+     */
     private String privateInstanceVariable = "privateInstanceVariable";
-    String defaultInstanceVariable = "defaultInstanceVariable";
+
+    /*
+     * Public scope means, this variable can be accesses from anywhere in the
+     *  program or project or module. (Any class, method, or block of code)
+     * Accessible to all classes:
+     *  - Even if the other class is in same package or different package
+     *  - Even if the class is a child class of this class
+     */
     public String publicInstanceVariable = "publicInstanceVariable";
 
+    /*
+     * Default or package-private scope (without any access modifier) means,
+     * this variable can be accesses from anywhere in the current class and
+     * any class in the same package.(Any class, method, or block of code in
+     * the current package only).
+     * Not accessible outside package:
+     *  - Even if the class is a child class of this class
+     *  - Even if the other class is in sub-package
+     */
+    String defaultInstanceVariable = "defaultInstanceVariable";
+
+    /*
+     * Protected scope means, this variable can be accessed from anywhere in
+     * the current class and current package but not accessible to any other
+     * class in different package if it is not a child class.
+     * Accessible only in the current package and its subclasses, i.e.:
+     * - Classes in the current package
+     * - Classes in the different package only if the class is subclass
+     *   of current class
+     */
+    protected String protectedInstanceVariable = "protectedInstanceVariable";
+
+    /**
+     * @param args the passed input string array argument
+     */
     public static void main(String[] args) {
         System.out.println("\nInstanceVariables.main():");
         InstanceVariables instance1 = new InstanceVariables();
         System.out.println(instance1.privateInstanceVariable);
         System.out.println(instance1.publicInstanceVariable);
         System.out.println(instance1.defaultInstanceVariable);
+        System.out.println(instance1.protectedInstanceVariable);
 
-        // Modify privateInstanceVariable for testing in anotherStaticMethod
-        instance1.privateInstanceVariable += "+main";
-        System.out.println(instance1.privateInstanceVariable);
+        // Modify publicInstanceVariable for testing in anotherStaticMethod
+        instance1.publicInstanceVariable += "+main";
+        System.out.println(instance1.publicInstanceVariable);
         anotherStaticMethod();
     }
 
+    /**
+     *
+     */
     private static void anotherStaticMethod() {
         System.out.println("\nInstanceVariables.anotherStaticMethod():");
         InstanceVariables instance1 = new InstanceVariables();
         System.out.println(instance1.privateInstanceVariable);
         System.out.println(instance1.publicInstanceVariable);
         System.out.println(instance1.defaultInstanceVariable);
+        System.out.println(instance1.protectedInstanceVariable);
         /*
          * Even though the main method has modified the
          * privateInstanceVariable, you can see that it is not changed in
@@ -46,25 +114,38 @@ public class InstanceVariables {
          *   They each have their own copy of all instance variables.
          */
         instance1.instanceMethod();
+        System.out.println(instance1.publicInstanceVariable);
+
     }
 
+    /**
+     * Notice that I have used variable names without any instance name
+     * with it, this means: they belong to current instance, on which the
+     * method is called, which is "instance1" from anotherStaticMethod above.
+     */
     private void instanceMethod() {
         System.out.println("\nInstanceVariables.instanceMethod():");
         System.out.println(privateInstanceVariable);
         System.out.println(publicInstanceVariable);
         System.out.println(defaultInstanceVariable);
-        // Modify privateInstanceVariable for testing in anotherInstanceMethod
-        privateInstanceVariable += "+instanceMethod";
-        System.out.println(privateInstanceVariable);
-
-        anotherInstanceMethod();
+        System.out.println(protectedInstanceVariable);
+        // Modify publicInstanceVariable for testing in anotherInstanceMethod
+        publicInstanceVariable += "+instanceMethod";
+        System.out.println(publicInstanceVariable);
+        // anotherInstanceMethod();
     }
 
+    /**
+     * Method to how instance variables are affected if they belong to the same
+     * instance, even though the actual instance is not in the scope of this
+     * method.
+     */
     private void anotherInstanceMethod() {
         System.out.println("\nInstanceVariables.anotherInstanceMethod():");
         System.out.println(privateInstanceVariable);
         System.out.println(publicInstanceVariable);
         System.out.println(defaultInstanceVariable);
+        System.out.println(protectedInstanceVariable);
     }
 
 }
---
+++ src/a/basics/a/variables/scope/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * Package to demonstrate the scope of the variables(local, instance and
+ * static or class using below access modifiers:
+ * <uo>
+ * <li><code>public</code></li>
+ * <li><code>private</code></li>
+ * <li><code>default</code> or <code>package-private</code>
+ * (without any access modifier)</li>
+ * <li><code>protected</code></li>
+ * </uo><br />
+ * TODO: Insert link for correct package.
+ * <br />
+ * All the Access Modifiers will be covered in detail in package
+ * access_modifiers
+ */
+package a.basics.a.variables.scope;
